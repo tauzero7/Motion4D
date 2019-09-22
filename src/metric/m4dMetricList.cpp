5f3e98edb6086cfa644ee33f99d5e7daaba75357
Thomas <th.mueller73@web.de>
2019-09-22 20:19:22 +0200

    cpp

---
+++ src/metric/m4dMetricList.cpp
@@ -0,0 +1,14 @@
+
+#include "m4dMetricList.h"
+
+namespace m4d {
+
+const char* MetricList::stl_metric_names[] = {
+    "unknown",
+    "Minkowski",
+    "Schwarzschild",
+    "KerrBL",
+    "MorrisThorne"
+};
+
+}
